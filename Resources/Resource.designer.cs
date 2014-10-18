9eac648af7b5a5ca7c61443f437d237f5cf01d7b
Grumpy Trev <t.simmonds@virgin.net>
2014-10-18 16:02:09 +0100

    Single automatic scrolling line for game name

--- Resources/Resource.designer.cs
+++ Resources/Resource.designer.cs
@@ -66,44 +66,41 @@ namespace SliderCon
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f06000c
-			public const int action_about = 2131099660;
-			
-			// aapt resource value: 0x7f06000a
-			public const int action_more = 2131099658;
-			
 			// aapt resource value: 0x7f06000b
-			public const int action_select_game = 2131099659;
-			
-			// aapt resource value: 0x7f06000d
-			public const int action_settings = 2131099661;
-			
-			// aapt resource value: 0x7f060007
-			public const int backButton = 2131099655;
+			public const int action_about = 2131099659;
 			
 			// aapt resource value: 0x7f060009
-			public const int gameName = 2131099657;
+			public const int action_more = 2131099657;
+			
+			// aapt resource value: 0x7f06000a
+			public const int action_select_game = 2131099658;
+			
+			// aapt resource value: 0x7f06000c
+			public const int action_settings = 2131099660;
+			
+			// aapt resource value: 0x7f060004
+			public const int backButton = 2131099652;
 			
 			// aapt resource value: 0x7f060008
-			public const int gameboard = 2131099656;
+			public const int gameName = 2131099656;
+			
+			// aapt resource value: 0x7f060007
+			public const int gameboard = 2131099655;
 			
 			// aapt resource value: 0x7f060005
 			public const int minCount = 2131099653;
 			
-			// aapt resource value: 0x7f060004
-			public const int minCountLabel = 2131099652;
-			
-			// aapt resource value: 0x7f060003
-			public const int movesCount = 2131099651;
+			// aapt resource value: 0x7f060006
+			public const int minCountLabel = 2131099654;
 			
 			// aapt resource value: 0x7f060002
-			public const int movesCountLayout = 2131099650;
+			public const int movesCount = 2131099650;
 			
 			// aapt resource value: 0x7f060001
 			public const int movesLabel = 2131099649;
 			
-			// aapt resource value: 0x7f060006
-			public const int resetButton = 2131099654;
+			// aapt resource value: 0x7f060003
+			public const int resetButton = 2131099651;
 			
 			// aapt resource value: 0x7f060000
 			public const int topStatusLayout = 2131099648;
